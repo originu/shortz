ff19ead8d9b6db47f87683102b7dce2e10ebbfdb
kevin <originu@gmail.com>
2023-03-31 22:07:07 +0900

    check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 export ECS_INSTANCE_IP_TASK=$(curl --retry 5 -connect-timeout 3 -s ${ECS_CONTAINER_METADATA_URI})
 export ECS_INSTANCE_HOSTNAME=$(cat /proc/sys/kernel/hostname)
 export ECS_INSTANCE_IP_ADDRESS=$(echo ${ECS_INSTANCE_IP_TASK} | python3 -c "import sys, json; print(json.load(sys.stdin)['Networks'][0]['IPv4Addresses'][0])")
+echo ${ECS_CONTAINER_METADATA_URI})
 echo "${ECS_INSTANCE_IP_ADDRESS} ${ECS_INSTANCE_HOSTNAME}" | tee -a /etc/hosts
 echo "ECS_INSTANCE_HOSTNAME: " ${ECS_INSTANCE_HOSTNAME}
 echo "ECS_INSTANCE_IP_ADDRESS: " ${ECS_INSTANCE_IP_ADDRESS}
